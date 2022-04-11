7737645eddb8fb4d8db7b11cfa65aa8b0bb65db0
techiethemastermind <grishabelinsky@hotmail.com>
2022-04-12 07:55:32 +0800

    update

--- app/Http/Controllers/Dashboard/HomeController.php
+++ app/Http/Controllers/Dashboard/HomeController.php
@@ -25,10 +25,14 @@ class HomeController extends Controller
      */
     public function index()
     {
-        dd('dfdfdf');
-        $total_num  = Subscriber::all()->count();
-        $active_num = Subscriber::where('status', 1)->count();
-        $deactive_num = Subscriber::where('status', 0)->count();
+        // $total_num  = Subscriber::all()->count();
+        // $active_num = Subscriber::where('status', 1)->count();
+        // $deactive_num = Subscriber::where('status', 0)->count();
+
+        $total_num  = 200000;
+        $active_num = 200000;
+        $deactive_num = 0;
+
         return view('home', compact('total_num', 'active_num', 'deactive_num'));
     }
 }

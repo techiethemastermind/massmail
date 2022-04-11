9241fe7cbdce1c0613f32d0223cd571f4fbc30aa
techiethemastermind <grishabelinsky@hotmail.com>
2022-04-12 07:54:15 +0800

    update

--- app/Http/Controllers/Dashboard/HomeController.php
+++ app/Http/Controllers/Dashboard/HomeController.php
@@ -25,8 +25,8 @@ class HomeController extends Controller
      */
     public function index()
     {
+        dd('dfdfdf');
         $total_num  = Subscriber::all()->count();
-        dd($total_num);
         $active_num = Subscriber::where('status', 1)->count();
         $deactive_num = Subscriber::where('status', 0)->count();
         return view('home', compact('total_num', 'active_num', 'deactive_num'));
